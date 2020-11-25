816c5efe63269a08596e10d86818684e9a7ca097
icarus44_zer0 <42955626+icarus44-zer0@users.noreply.github.com>
2020-11-25 12:38:03 -0800

    Working Login and Payload files - exe to fast

--- sshpoc.py
+++ sshpoc.py
@@ -1,17 +1,32 @@
 import sys
 import os
-from paramiko import SSHClient
+import paramiko 
+import time
 
-print ('Target Adress:',  str(sys.argv))
+# open credentials.txt file for reading 
+with open('credentials.txt') as f:
+    creds = [line.rstrip() for line in f]
 
-server = sys.argv[0]
-username = sys.argv[1]
-password = sys.argv[2]
-cmd_to_execute =  sys.argv[3]
+# open payload.txt file for reading 
+with open('payload.txt') as f:
+    payload = [line.rstrip() for line in f]
 
+# connect to host with creds in payload.txt
+host = creds[0]
+port = 22
+username = creds[1]
+password = creds[2]
 
-ssh = SSHClient()
-ssh.connect(server, username=username, password=password)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
-print(ssh_stdout)
+# ssh connection
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, password)
 
+# payload all the things 
+for command in payload:
+    stdin, stdout, stderr = ssh.exec_command(command)
+    time.sleep(1)
+    lines = stdout.readlines()
+    print(lines)
+
+# ssh.close()
