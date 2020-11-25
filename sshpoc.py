a734f7f29b6e581b1f84722f12204501cc438173
icarus44_zer0 <42955626+icarus44-zer0@users.noreply.github.com>
2020-11-25 11:32:15 -0800

    First attemtp

--- sshpoc.py
+++ sshpoc.py
@@ -1,1 +1,17 @@
-sshpoc.py
+import sys
+import os
+from paramiko import SSHClient
+
+print ('Target Adress:',  str(sys.argv))
+
+server = sys.argv[0]
+username = sys.argv[1]
+password = sys.argv[2]
+cmd_to_execute =  sys.argv[3]
+
+
+ssh = SSHClient()
+ssh.connect(server, username=username, password=password)
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
+print(ssh_stdout)
+
